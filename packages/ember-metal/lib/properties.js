7c26d67e1668e449d563fedc7d915df1f541f2d8
Godfrey Chan <godfreykfc@gmail.com>
2018-01-30 08:47:29 -0600

    [BUGFIX beta] Use "computed properties" in the msg

--- packages/ember-metal/lib/properties.js
+++ packages/ember-metal/lib/properties.js
@@ -82,11 +82,12 @@ if (EMBER_METAL_ES5_GETTERS) {
   // development to aid in development asertions. Production builds of
   // ember strip this entire branch out.
   let messageFor = function(obj, keyName, property, value) {
-    return `You attempted to access the \`${keyName}.${String(property)}\` property ` +
-      `(of ${obj}). Due to certain internal implementation details of Ember, ` +
-      `the \`${keyName}\` property previously contained an internal "descriptor" ` +
-      `object (a private API), therefore \`${keyName}.${String(property)}\` would have ` +
-      `been \`${String(value).replace(/\n/g, ' ')}\`. This internal implementation ` +
+    return `You attempted to access \`${keyName}.${String(property)}\` (of ${obj}), ` +
+      `where \`${keyName}\` is a computed property.\n\n` +
+      `Due to certain internal implementation details of Ember, the \`${keyName}\` ` +
+      `property previously contained an internal "descriptor" object (a private API), ` +
+      `therefore \`${keyName}.${String(property)}\` would have been ` +
+      `\`${String(value).replace(/\n/g, ' ')}\`. This internal implementation ` +
       `detail was never intended to be a public (or even intimate) API.\n\n` +
       `This internal implementation detail has now changed and the (still private) ` +
       `"descriptor" object has been relocated to the object's "meta" (also a ` +
